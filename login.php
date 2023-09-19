e30a12b57842ed61bb90617d4ae18a38ff5fdb4e
katheren salom <katherensalom@gmail.com>
2023-09-18 21:41:54 -0400

    actualizando

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS v5.0.2 -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/login.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
     <!-- Theme style -->
@@ -80,69 +80,38 @@
                 
             }
     ?>
+    
      <!-- Formulario Login -->
-     <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-md-4 col-md-offset-4">
-          <!-- Margen superior (css personalizado )-->
-          <div class="spacing-1"></div>
-
-          <?php
+     <div class="login">
+      <?php
             if (isset($error)) {
               echo  '<div class="alert alert-danger alert-dismissible" role="alert">
                       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <strong>'.$error. '</strong>
                      </div>' ;
             }
-           ?>    
+           ?>  
+        <h1>Sistema de Reciclados</h1>
 
-          <!-- Estructura del formulario -->
-            <form method="post">
+        <form action="" method="post">
+            <label for="email">
+              <i class="fa fa-user"></i>
 
-                <fieldset>
-
-                    <legend class="center">Login</legend>
-
-                    <!-- Caja de texto para usuario -->
-                    <label class="sr-only" for="email">Usuario</label>
-                    <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                    <input type="email" class="form-control" name="email" placeholder="Ingresa tu email">
-                    </div>
-
-                    <!-- Div espaciador -->
-                    <div class="spacing-2"></div>
-
-                    <!-- Caja de texto para la clave-->
-                    <label class="sr-only" for="contrasenya">Contraseña</label>
-                    <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-lock"></i></div>
-                    <input type="password" autocomplete="off" class="form-control" name="contrasenya" placeholder="Ingresa tu usuario">
-                    </div>
-                    <div class="row">
-                    <div class="col-xs-8 col-xs-offset-2">
-                        <div class="spacing-2"></div>
-                        <input type="submit" class="btn btn-primary btn-block" value="Iniciar sesion">
-                    </div>
-                    </div>
-
-                </fieldset>
-            </form>
-
-        </div>
-      </div>
+            </label>
+            <input type="email" name="email"
+            placeholder="Usuario"  required>
+            <br>
+            <label for="contrasenya">
+              <i class="fa fa-lock"></i>
+            </label>
+            <input type="password" name="contrasenya"
+            placeholder="Contraseña" required>
+            <input type="submit" value="Acceder">
+        </form>
     </div>
 
-<footer class="container-fluid bg-blue fixed-bottom">
-        <div class="row">
-            <div class="col-md text-light text-center py-3">
-                Desarrollado por Katheren Salom
-            </div>
-        </div>
-    </footer>
-      
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>-->
   </body>
 </html>
